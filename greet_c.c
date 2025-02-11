66ab604bae314d227c6089a277901b8a4ef1d62c
avi-oraon <avi.oraon.edgeai@gmail.com>
2025-02-11 14:19:36 +0530

    c files uploaded
    as asked c file is being uploaded

---
+++ greet_c.c
@@ -0,0 +1,6 @@
+#include <stdio.h> // Include standard input/output library
+
+int main() {  // The main function where execution begins
+  printf("Hello, World!\n"); // Print text to the console
+  return 0; // Indicate successful program termination
+}
